da3eee9ba0130ff7ff6c5fbda92f3c9ba58958dc
kingranzz <156513688+kingranzz@users.noreply.github.com>
2025-04-15 23:35:56 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  BOT_TOKEN: "7939910743:AAFkyMkb8zlFtg3i1nK2PTHOX0cl7a-D5xc", 
-  OWNER_ID: "5965707646", 
-  allowedGroupIds: [],
+  BOT_TOKEN: "8005553720:AAGZJP2A1-FekuG6W_5AU4dovWy19SuKleM", // Token bot Telegram
+  OWNER_ID: "5965707646", // ID pemilik bot
+  allowedGroupIds: [-1002401306074, -1002361556670, -1002386380830, -1002415460020, -4514397992], // ID grup yang diizinkan
 };
