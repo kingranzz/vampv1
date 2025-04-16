1a221d1f924f774af306eacfeec871099565784b
kingranzz <156513688+kingranzz@users.noreply.github.com>
2025-04-16 20:49:22 +0800

    Add files via upload

---
+++ VampConfig.js
@@ -0,0 +1,6 @@
+const global = {
+    owner: [7797598838], // ganti jadi id mu
+    botToken: "7754731676:AAE474k44A4coN6HtkigAX6nyr0RgjKo8S4", //isi make token bot mu
+}
+
+module.exports = global;
