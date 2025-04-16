ee085d96ff229a3b3019749184fa13257968ef65
kingranzz <156513688+kingranzz@users.noreply.github.com>
2025-04-16 20:48:14 +0800

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  BOT_TOKEN: "8005553720:AAGZJP2A1-FekuG6W_5AU4dovWy19SuKleM", // Token bot Telegram
-  OWNER_ID: "5965707646", // ID pemilik bot
-  allowedGroupIds: [-1002401306074, -1002361556670, -1002386380830, -1002415460020, -4514397992], // ID grup yang diizinkan
-};
