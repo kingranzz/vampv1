98184aab9b0c331836724eb992045165905ceba8
kingranzz <156513688+kingranzz@users.noreply.github.com>
2025-06-29 00:58:27 +0800

    Update VampConfig.js

--- VampConfig.js
+++ VampConfig.js
@@ -1,6 +1,6 @@
 const global = {
     owner: [5965707646], // ganti jadi id mu
-    botToken: "7549474841:AAG78UjaCRBwE63TNlNst_ZEc-hjJcwREjU", //isi make token bot mu
+    botToken: "7688717854:AAEV53wtuq30LKHLo5iwUaKsXxEIDFG3nYE", //isi make token bot mu
 }
 
 module.exports = global;
