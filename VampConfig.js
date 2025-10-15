0266c5a8b0c598d4d765a3bda0c06e1f566caf7b
kingranzz <156513688+kingranzz@users.noreply.github.com>
2025-10-15 17:31:13 +0700

    Delete VampConfig.js

--- VampConfig.js
+++
@@ -1,6 +0,0 @@
-const global = {
-    owner: [5965707646,8094689641,1972929194],
-    botToken: "7549474841:AAG78UjaCRBwE63TNlNst_ZEc-hjJcwREjU", //isi make token bot mu
-}
-
-module.exports = global;
