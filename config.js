2e1036a564fe090c15bd779f717180ffc2c7e74f
kingranzz <156513688+kingranzz@users.noreply.github.com>
2025-10-15 17:34:17 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    
+    ownerId: 8219845021, // ID LU
+    
+    telegramBotToken: "8136946939:AAHiZTZyXDywVJ57Ar028D8O0V5zymmRt6o", // TOKEN LU
+
+    sessionName: "session",
+
+
+    photoStart: "https://files.catbox.moe/y72ctj.jpg",
+
+    message: {
+        owner: `🚫 Cuma owner yang bisa, bos!`,
+        premium: `💎 Khusus member premium, bos!`,
+        wait: `⏳ Otw boskuu, sabar ya...`,
+        error: `⚠️ Anjir, error. Coba lagi ntar.`,
+        waNotConnected: `⚠️ Waduh, WA-nya belum nyambung bos. /pairing dulu lah.`
+    },
+
+    settings: {
+        namabot: "Cek Bio By Sahroni",
+        footer: "Powered By Sahroni",
+        cekBioBatchSize: 15 // Jumlah nomor yang dicek bersamaan.
+    }
+};
